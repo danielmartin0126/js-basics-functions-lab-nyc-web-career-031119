02ec677113c73936ad5cebf0593543674fb7e953
Danny Martin <danielmartin0126@gmail.com>
2019-04-21 22:31:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function distanceTravelledInFeet(start, end) {
 }
 
 function calculatesFarePrice(start,end) {
-  dist = distanceTravelledInFeet(start,end)
+  dist = distanceTravelledInFeet(start, end)
   let response
   
   switch (dist){
